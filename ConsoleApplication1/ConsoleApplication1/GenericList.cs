922f9d88d291af51c8b6e29c8a45260bafcac07b
Matthew <mdhays1995@gmail.com>
2016-06-08 22:14:40 -0500

    Played around with generics for a while to get a feel for how they can be used.

---
+++ ConsoleApplication1/ConsoleApplication1/GenericList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{   
+    public class GenericList<T>
+    {
+        public void Evaluate(T input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Generic types
+            GenericList<int> list1 = new GenericList<int>();
+            GenericList<string> list2 = new GenericList<string>();
+            GenericList<bool> list3 = new GenericList<bool>();
+
+            list1.Evaluate(1);
+            list2.Evaluate("Hello, as you can see, I'm a string.");
+            list3.Evaluate(true);
+
+            Console.ReadLine();
+        }
+    }
+}

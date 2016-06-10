c211f4529b4968188927fd6176bea8543ad6799f
Matthew <mdhays1995@gmail.com>
2016-06-09 21:48:17 -0500

    Played around with expression lambdas.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,6 +8,14 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        //The 'del' in this delegate invocation is just a name. You can rename it if need be.
+
+        delegate int del(int i);
+        delegate int twodel(int a);
+        delegate void TestDelegate(string s);
+        delegate void thirddel(string t);
+
+
         static void Main(string[] args)
         {   
             //Generic types
@@ -18,6 +26,21 @@ namespace ConsoleApplication1
             list1.Evaluate(1);
             list2.Evaluate("Hello, as you can see, I'm a string.");
             list3.Evaluate(true);
+
+            //The lambda expression x => x * x specifies a parameter that’s named x and returns the value of x squared.
+            //A lambda expression with an expression on the right side of the => operator is called an expression lambda. Expression lambdas are used extensively in the construction of Expression Trees (C# and Visual Basic).
+            //An expression lambda returns the result of the expression and takes the following basic form: (input parameters) => expression
+
+            del myDelegate = x => x * x;
+
+            twodel mySecondDelegate = y => y * y;
+
+            int c = mySecondDelegate(5);
+
+            int j = myDelegate(6);
+
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("j = " + j);
 
             Console.ReadLine();
         }
